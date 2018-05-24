4c6bda302a7db9d7aeb05844dade87291fe20e35
Unknown <rileyharper15@gmail.com>
2018-05-24 17:36:07 -0600

    Animals

---
+++ 1600Scripting/Assets/Scripts/Animals/Bird.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class Bird : Animal {
+
+    public int FeatherCount = 300;
+    public Color HairColor = Color.black;
+}
