cb30acae8b1de6d30774edbeb4a7b25c8d87ae92
Unknown <rileyharper15@gmail.com>
2018-05-24 17:36:07 -0600

    Animals/PowerUps

---
+++ 1600Scripting/Assets/Scripts/Animals/Lizard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class Lizard : Animal {
+    public int ScaleCount = 400;
+    public int TailCount = 1;
+    public Color ScaleColor = Color.black;
+}
