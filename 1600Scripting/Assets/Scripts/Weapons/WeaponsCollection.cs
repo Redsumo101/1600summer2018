ea258fec0bf217f540de70586d23194baf013751
Unknown <rileyharper15@gmail.com>
2018-06-25 11:25:34 -0600

    Gun

---
+++ 1600Scripting/Assets/Scripts/Weapons/WeaponsCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsCollection : MonoBehaviour {
+    public List<WeaponObject> Weapons;
+    public void CollectWeapon( WeaponObject weapon)
+    {
+        Weapons.Add(weapon);
+        foreach (var item in Weapons)
+        {
+            Debug.Log(item);
+            if (item.name == "Sword")
+            {
+                Debug.Log("You have the best weapon");
+                    }
+        }
+    }
+}
