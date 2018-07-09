3e70e698df3bac8d3e4209484504093b779c2f03
Unknown <rileyharper15@gmail.com>
2018-07-08 19:27:00 -0600

    Armor

---
+++ 1600Scripting/Assets/Scripts/Armor/Armor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armor : MonoBehaviour
+{
+
+	public enum ArmorType
+
+	{
+		Helmet,
+		Legs,
+		Gloves,
+		Arms,
+		BreastPlate,
+	}
+
+	public ArmorObject[] ArmorObjects;
+
+	public ArmorType CurrentArmor;
+
+
+	void Update()
+	{
+		switch (CurrentArmor)
+		{
+			case ArmorType.Helmet:
+				Debug.Log(ArmorObjects[0].name);
+				Debug.Log(ArmorObjects[0].TakeDamage());
+				break;
+			case ArmorType.Legs:
+				Debug.Log(ArmorObjects[1].name);
+				Debug.Log(ArmorObjects[1].TakeDamage());
+				break;
+			case ArmorType.Gloves:
+				Debug.Log(ArmorObjects[2].name);
+				Debug.Log(ArmorObjects[2].TakeDamage());
+				break;
+			case ArmorType.Arms:
+				Debug.Log(ArmorObjects[3].name);
+				Debug.Log(ArmorObjects[3].TakeDamage());
+				break;
+			case ArmorType.BreastPlate:
+				Debug.Log(ArmorObjects[4].name);
+				Debug.Log(ArmorObjects[4].TakeDamage());
+				break;
+		}
+	}
+}
