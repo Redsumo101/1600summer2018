f7be1b1c4a7e374e3d7603fbb6ea4a69badae878
Unknown <rileyharper15@gmail.com>
2018-07-11 11:27:10 -0600

    AI

---
+++ 1600Scripting/Assets/Scripts/AI/AIMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIMovement : MonoBehaviour
+{
+	private NavMeshAgent Agent;
+	public Transform Destination;
+	public AIControl Control;
+	
+	
+	private void Start ()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+		Control.ConfigAI(Agent);
+	}
+	
+	
+	private void Update ()
+	{
+		Agent.destination = Destination.position;
+	}
+}
---
+++ 1600Scripting/Assets/Scripts/AI/AISpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AISpawner : MonoBehaviour
+{
+
+	public GameObject Ai;
+	public Transform Destination;
+	private void Start ()
+	{
+		StartCoroutine(StartSpawn());
+	}
+
+	IEnumerator StartSpawn()
+	{
+		while (true)
+		{
+			GameObject newAI = Instantiate(Ai);
+			newAI.GetComponent<AIMovement>().Destination = Destination;
+			yield return new WaitForSeconds(2);
+
+		}
+	}
+}
