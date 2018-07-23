6658c7c2382a480285c963e3ec6a999ed259b0f2
Unknown <rileyharper15@gmail.com>
2018-07-22 23:16:46 -0600

    Animation with scripts

---
+++ 1600Scripting/Assets/Scripts/Animation/BounceOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnClick : MonoBehaviour
+{
+    private Animator anims;
+    public IntData CoinCollection;
+
+    private void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+   private void OnMouseDown()
+    {
+        anims.SetTrigger("Can Bounce");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anims.SetTrigger("Can Bounce");
+    }
+
+    public void EndAnims()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<ParticleSystem>().Emit(20);
+        CoinCollection.Value++;
+    }
+}
