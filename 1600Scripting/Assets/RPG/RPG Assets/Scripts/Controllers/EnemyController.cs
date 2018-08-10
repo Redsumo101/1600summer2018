c79db0833c6394121332f57b5a3309794bc7f139
Unknown <rileyharper15@gmail.com>
2018-08-10 17:51:13 -0600

    Combat/Stats

---
+++ 1600Scripting/Assets/RPG/RPG Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+
+	public float lookRadius = 10f;
+
+	private Transform target;
+	private NavMeshAgent agent;
+
+	private CharacterCombat combat;
+	// Use this for initialization
+	void Start ()
+	{
+		target = PlayerManager.instance.player.transform;
+		agent = GetComponent<NavMeshAgent>();
+		combat = GetComponent<CharacterCombat>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		float distance = Vector3.Distance(target.position, transform.position);
+
+		if (distance <= lookRadius)
+		{
+			CharacterStats targetStats = target.GetComponent<CharacterStats>();
+			if (targetStats != null)
+			{
+				combat.Attack(targetStats);
+			}
+			
+			agent.SetDestination(target.position);
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+	Gizmos.color=Color.red;
+		Gizmos.DrawWireSphere(transform.position, lookRadius);
+	}
+}
